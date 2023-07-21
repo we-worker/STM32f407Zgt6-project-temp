742e32888d1a297608ceacfa35828ad631757e0b
darkarc <2350230863@qq.com>
2023-07-21 19:15:33 +0800

    git优化

--- Project/OBJ/startup_stm32f40_41xxx.d
+++
@@ -1,1 +0,0 @@
-.\obj\startup_stm32f40_41xxx.o: ..\Startup_config\startup_stm32f40_41xxx.s
--- USER/timer4.h
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-void TIM4_Init(u16 auto_data, u16 fractional);
-void TIM4_Init2(u16 auto_data, u16 fractional);
-void TIM3_Init(u16 auto_data, u16 fractional);
-void EXTIX_Init(void);
-
-extern uint32_t Frequency;
-
